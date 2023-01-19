ddbfe2320e0bb636d085cee614478820cc4ce7ef
Jyoti Nath <jyoti.nath@publicissapient.com>
2023-01-19 09:52:57 +0530

    git file added

---
+++ MyPractice/gitPractice.js
@@ -0,0 +1,19 @@
+const jonasArray = [
+  'Jonas',
+  'Schmedtman',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+];
+
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtman',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friend: ['Michael', 'Peter', 'Steven'],
+};
+
+console.log(jonas);
+console.log('-----');
+console.log(jonas.friend[0]);
